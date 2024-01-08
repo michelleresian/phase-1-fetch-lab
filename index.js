5ab34e9c52e7dbf5544904205c8f3daf04e2a7a0
Michelle Resian <you@example.com>
2024-01-08 11:21:33 +0300

    fetch-lab

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderBooks(data);
+    })
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
